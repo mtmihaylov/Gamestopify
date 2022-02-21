7ecb64c00acd43fc99c370151c4049bcefb0a14a
Mihail Mihaylov <mihail.mihailov222@abv.bg>
2022-02-21 22:39:59 +0200

    Added User reference to Product Schema

--- backend/models/product.js
+++ backend/models/product.js
@@ -89,6 +89,11 @@ const productSchema = new mongoose.Schema({
   createAt: {
     type: Date,
     default: Date.now
+  },
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: true
   }
 });
 
