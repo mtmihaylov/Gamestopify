6ef0b80b0e1e1deb40e568827281cc3258ea4388
Mihail Mihaylov <mihail.mihailov222@abv.bg>
2022-03-03 20:31:07 +0200

    Add order details functionalitites
    Add get single order details functionality
    Add get logged in user orders functionality

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { newOrder } = require('../controllers/orderController');
+const { newOrder, getOrder, myOrders } = require('../controllers/orderController');
 
 const { isAuthenticated, authorizeRoles } = require('../middlewares/auth');
 
 router.route('/order/new').post(isAuthenticated, newOrder);
+router.route('/order/:id').get(isAuthenticated, getOrder);
+router.route('/myprofile/orders').get(isAuthenticated, myOrders);
 
 module.exports = router;
