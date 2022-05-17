4738e6307aac2dcb4efa4f58450fa646595e370d
Mihail Mihaylov <mihail.mihailov222@abv.bg>
2022-05-17 17:55:37 +0300

    Implement React Router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import Header from './components/layout/Header';
-import Footer from './components/layout/Footer';
-import Home from './components/Home';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Home/>
-      <Footer/>
+      <Header />
+      <Routes>
+        {/* <div className="container container-fluid"> */}
+          <Route path="/" element={<Home />} />
+        {/* </div> */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
