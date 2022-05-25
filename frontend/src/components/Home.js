60ac6e91ec8f19ee4750948c0a21c0e582fe31b3
Mihail Mihaylov <mihail.mihailov222@abv.bg>
2022-05-25 23:47:26 +0300

    Add Product component
    Add Product component and setup the Home component to render actual products

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MetaData from "./layout/MetaData";
 
+import { useDispatch, useSelector } from "react-redux";
+import { getProducts } from "../actions/productsActions";
+
+import Product from './Product/Product';
+
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const { products, productsCount, loading, error } = useSelector(
+    (state) => state.products
+  );
+
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
+
   return (
     <div className="container container-fluid">
       <MetaData title="Your shop for gaming, electronics and pop culture | GameStop" />
@@ -10,29 +25,9 @@ const Home = () => {
 
       <section id="products" className="container mt-5">
         <div className="row">
-          <div className="col-sm-12 col-md-6 col-lg-3 my-3">
-            <div className="card p-3 rounded">
-              <img
-                className="card-img-top mx-auto"
-                src="https://m.media-amazon.com/images/I/617NtexaW2L._AC_UY218_.jpg"
-              />
-              <div className="card-body d-flex flex-column">
-                <h5 className="card-title">
-                  <a href="/#">128GB Solid Storage Memory card - SanDisk Ultra</a>
-                </h5>
-                <div className="ratings mt-auto">
-                  <div className="rating-outer">
-                    <div className="rating-inner"></div>
-                  </div>
-                  <span id="no_of_reviews">(5 Reviews)</span>
-                </div>
-                <p className="card-text">$45.67</p>
-                <a href="/#" id="view_btn" className="btn btn-block">
-                  View Details
-                </a>
-              </div>
-            </div>
-          </div>
+          {products && products.map((product) => (
+              <Product key={product._id} product={product}/>
+            ))}
         </div>
       </section>
     </div>
