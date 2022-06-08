7ce05e6452896fda664daa94373863f4080674dc
Mihail Mihaylov <mihail.mihailov222@abv.bg>
2022-06-08 15:44:33 +0300

    Fix path to Product Details

--- frontend/src/components/Product/Product.js
+++ frontend/src/components/Product/Product.js
@@ -5,12 +5,12 @@ const Product = ({ product, col }) => {
   return (
     <div className={`col-sm-12 col-md-6 col-lg-${col} my-3`}>
       <div className="card p-3 rounded shadow bg-white">
-        <Link className="card-img-top mx-auto" to={`product/${product._id}`}>
+        <Link className="card-img-top mx-auto" to={`/product/${product._id}`}>
           <img className="card-img-top mx-auto" src={product.images[0].url} alt="product preview"/>
         </Link>
         <div className="card-body d-flex flex-column">
           <h5 className="card-title">
-            <Link to={`product/${product._id}`}>{product.name}</Link>
+            <Link to={`/product/${product._id}`}>{product.name}</Link>
           </h5>
           <div className="ratings mt-auto">
             <div className="rating-outer">
@@ -23,7 +23,7 @@ const Product = ({ product, col }) => {
           </div>
           <p className="card-text">${product.price}</p>
           <Link
-            to={`product/${product._id}`}
+            to={`/product/${product._id}`}
             id="view_btn"
             className="btn btn-block"
           >
