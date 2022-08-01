c85899f765ab2c2789078cc6384816c61df3592e
Mihail Mihaylov <mihail.mihailov222@abv.bg>
2022-08-01 20:31:38 +0300

    Remove Out of Stock Card

--- frontend/src/components/admin/Dashboard.js
+++ frontend/src/components/admin/Dashboard.js
@@ -21,12 +21,12 @@ const Dashboard = ({ isAdmin }) => {
   const { orders, totalAmount } = useSelector((state) => state.allOrders);
   const { users } = useSelector((state) => state.allUsers);
 
-  let outOfStockProductsCount = 0;
-  products.forEach((p) => {
-    if (p.stock === 0) {
-      outOfStockProductsCount++;
-    }
-  });
+  // let outOfStockProductsCount = 0;
+  // products.forEach((p) => {
+  //   if (p.stock === 0) {
+  //     outOfStockProductsCount++;
+  //   }
+  // });
 
   useEffect(() => {
     // if (!isAdmin) {
@@ -66,7 +66,7 @@ const Dashboard = ({ isAdmin }) => {
             </div>
 
             <div className="row pr-4">
-              <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="col-xl-4 col-sm-6 mb-3">
                 <div className="card text-white bg-success o-hidden h-100">
                   <div className="card-body">
                     <div className="text-center card-font-size">
@@ -86,7 +86,7 @@ const Dashboard = ({ isAdmin }) => {
                 </div>
               </div>
 
-              <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="col-xl-4 col-sm-6 mb-3">
                 <div className="card text-white bg-danger o-hidden h-100">
                   <div className="card-body">
                     <div className="text-center card-font-size">
@@ -106,7 +106,7 @@ const Dashboard = ({ isAdmin }) => {
                 </div>
               </div>
 
-              <div className="col-xl-3 col-sm-6 mb-3">
+              <div className="col-xl-4 col-sm-6 mb-3">
                 <div className="card text-white bg-info o-hidden h-100">
                   <div className="card-body">
                     <div className="text-center card-font-size">
@@ -126,7 +126,7 @@ const Dashboard = ({ isAdmin }) => {
                 </div>
               </div>
 
-              <div className="col-xl-3 col-sm-6 mb-3">
+              {/* <div className="col-xl-3 col-sm-6 mb-3">
                 <div className="card text-white bg-warning o-hidden h-100">
                   <div className="card-body">
                     <div className="text-center card-font-size">
@@ -144,7 +144,7 @@ const Dashboard = ({ isAdmin }) => {
                     </span>
                   </Link>
                 </div>
-              </div>
+              </div> */}
             </div>
           </div>
         )}
